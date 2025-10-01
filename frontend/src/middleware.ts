a0d1cacbc2af69ceb64ba405396e7ad4b368cecd
Rohit Yadav <rohityadav.se@gmail.com>
2025-10-01 20:26:59 +0545

    update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,18 +1,22 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { authClient } from "./auth/auth-client"
-import { headers } from "next/headers"
 
 export async function middleware(req: NextRequest) {
+	// grab headers directly from the request
+	const headersObj = Object.fromEntries(req.headers.entries())
+
 	const { data, error } = await authClient.getSession({
 		fetchOptions: {
-			headers: await headers(),
+			headers: headersObj,
 		},
 	})
+
 	if (error) {
 		console.error("Error fetching session:", error)
 		throw new Error("Failed to fetch session")
 	}
+
 	if (!data?.session) {
 		const url = req.nextUrl.clone()
 		url.pathname = "/login"

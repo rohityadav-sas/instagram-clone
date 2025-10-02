f7ced48393a484b42836249d6b58cca9f41059f1
Rohit Yadav <rohityadav.se@gmail.com>
2025-10-02 18:37:43 +0545

    final fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,28 +2,8 @@
 import Sidebar from "@/components/Sidebar"
 import Feed from "@/components/Feed"
 import Suggestions from "@/components/Suggestions"
-import axios_instance from "@/config/axios"
-import { useEffect } from "react"
-import { useUserStore } from "@/store/store"
-import { useSearchParams } from "next/navigation"
 
 export default function Home() {
-	const setUser = useUserStore((state) => state.setUser)
-	const searchParams = useSearchParams()
-	useEffect(() => {
-		const authToken = searchParams.get("authToken")
-		if (authToken) {
-			localStorage.setItem("bearer_token", authToken)
-			window.history.replaceState({}, "", "/")
-			axios_instance.get("/users/me").then((response) => {
-				setUser(response.data.data)
-			})
-		} else {
-			axios_instance.get("/users/me").then((response) => {
-				setUser(response.data.data)
-			})
-		}
-	}, [])
 	return (
 		<div className="flex h-screen overflow-hidden">
 			<Sidebar />
--- frontend/src/components/RouteGuard.tsx
+++ frontend/src/components/RouteGuard.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import { usePathname, useRouter } from "next/navigation"
+import axios_instance from "@/config/axios"
+import { useUserStore } from "@/store/store"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
 
 // Define which routes need protection
@@ -22,11 +24,22 @@ export default function RouteGuard({
 	const router = useRouter()
 	const [loading, setLoading] = useState(true)
 
+	const setUser = useUserStore((state) => state.setUser)
+	const searchParams = useSearchParams()
+
 	useEffect(() => {
 		const token = localStorage.getItem("bearer_token")
-
-		// If current path starts with any protected route
 		const needsAuth = protectedRoutes.some((route) => {
+			if (pathname === "/") {
+				const authToken = searchParams.get("authToken")
+				if (authToken) {
+					localStorage.setItem("bearer_token", authToken)
+					window.history.replaceState({}, "", "/")
+					axios_instance.get("/users/me").then((response) => {
+						setUser(response.data.data)
+					})
+				} else return true
+			}
 			if (route === "/") return false
 			else return pathname?.startsWith(route)
 		})
